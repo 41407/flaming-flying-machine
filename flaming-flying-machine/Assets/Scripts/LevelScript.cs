4e51534e4f29747eb7510aec0a45f93c99ba1ae9
41407 <jimi.welling@cs.helsinki.fi>
2014-09-28 14:32:32 +0300

    Start player shooting

---
+++ flaming-flying-machine/Assets/Scripts/LevelScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelScript : MonoBehaviour
+{
+		public GameObject enemy;
+
+		// Use this for initialization
+		void Start ()
+		{
+				
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Time.frameCount % 100 == 0) {
+						Instantiate (enemy, new Vector3 (Random.Range (-40, 40), 10, 0), new Quaternion ());		
+				}
+		}
+}
