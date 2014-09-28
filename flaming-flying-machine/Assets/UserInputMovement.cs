eb95811e66764b87af2368a48ec720ca7c37b81c
41407 <jimi.welling@cs.helsinki.fi>
2014-09-28 13:35:47 +0300

    Add follow mouse movement

--- flaming-flying-machine/Assets/UserInputMovement.cs
+++ flaming-flying-machine/Assets/UserInputMovement.cs
@@ -3,22 +3,34 @@ using System.Collections;
 
 public class UserInputMovement : MonoBehaviour
 {
-
+		// Object that will be moved by this script
 		public GameObject objectToControl;
+		public GameObject objectToFollow;
+		public float followDeadzone;
 		public bool keyboardInput;
+		
+		// In units/s
 		public float speed;
+
+		// Range: [-1.0, 1.0] 
 		private float xMovement;
 		private float yMovement;
 
 		void Update ()
 		{
+				Vector2 movementVector;
 				if (keyboardInput) {
-						xMovement = Input.GetAxisRaw ("Horizontal");
-						yMovement = Input.GetAxisRaw ("Vertical");
+						movementVector = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 		
-						Vector2 movementVector = new Vector2 (xMovement, yMovement);
-		
-						objectToControl.transform.Translate (movementVector * speed * Time.deltaTime * Time.timeScale);
+
+				} else {
+						Vector2 distanceBetween = objectToFollow.transform.position - objectToControl.transform.position;
+						if (distanceBetween.magnitude > 1) {				
+								movementVector = distanceBetween.normalized;
+						} else {
+								movementVector = distanceBetween;
+						}
 				}
+				objectToControl.transform.Translate (movementVector * speed * Time.deltaTime * Time.timeScale);
 		}
 }
