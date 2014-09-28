bd3dc60f430ad24b2b699f1446ee3635706a6549
41407 <jimi.welling@cs.helsinki.fi>
2014-09-29 00:03:20 +0300

    Background images fine-tuned to perfection

---
+++ flaming-flying-machine/Assets/BackgroundGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundGenerator : MonoBehaviour
+{
+		public float speed;
+		public GameObject[] backgrounds;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Time.frameCount % 60 == 0) {
+						Vector3 pos = new Vector3 (Random.Range (-16, 16), 24, 2);
+						GameObject backdrop = (GameObject)Instantiate (backgrounds [Random.Range (0, backgrounds.Length - 1)], pos, Quaternion.identity);
+						backdrop.GetComponent<BackgroundMovement> ().ySpeed = Random.value * speed;
+				}
+		}
+}
---
+++ flaming-flying-machine/Assets/BackgroundMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMovement : MonoBehaviour
+{
+
+		public float rotationSpeed;
+		public float ySpeed;
+		public float scale;
+
+		void Start ()
+		{
+				rotationSpeed *= Random.value;
+				scale *= Random.value;
+				gameObject.transform.localScale = new Vector3 (scale, scale, 0);
+		}
+
+		void Update ()
+		{
+				gameObject.transform.Translate (Vector3.down * ySpeed * Time.deltaTime);
+				gameObject.transform.Rotate (0, 0, rotationSpeed);
+		}
+}
