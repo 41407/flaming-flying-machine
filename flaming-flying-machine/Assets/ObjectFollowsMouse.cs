b9a837b26abceab93315dcaf77309956fcb790e5
41407 <jimi.welling@cs.helsinki.fi>
2014-09-28 13:05:09 +0300

    Add mouse script

---
+++ flaming-flying-machine/Assets/ObjectFollowsMouse.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectFollowsMouse : MonoBehaviour {
+
+	public Camera camera;
+
+	void Update () {
+		Vector3 mouseScreenPosition = Input.mousePosition;
+		Vector3 mouseWorldPosition = camera.ScreenToWorldPoint (mouseScreenPosition);	
+		mouseWorldPosition.z = 0;
+		gameObject.transform.position = mouseWorldPosition;
+	}
+}
