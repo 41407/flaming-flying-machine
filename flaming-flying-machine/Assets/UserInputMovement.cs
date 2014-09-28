e30092c28d1f41f830e652121c66c829845e6303
41407 <jimi.welling@cs.helsinki.fi>
2014-09-28 12:51:22 +0300

    Add UserInputMovement

---
+++ flaming-flying-machine/Assets/UserInputMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInputMovement : MonoBehaviour
+{
+
+		public GameObject objectToControl;
+		public bool keyboardInput;
+		public float speed;
+		private float xMovement;
+		private float yMovement;
+
+		void Update ()
+		{
+				if (keyboardInput) {
+						xMovement = Input.GetAxisRaw ("Horizontal");
+						yMovement = Input.GetAxisRaw ("Vertical");
+		
+						Vector2 movementVector = new Vector2 (xMovement, yMovement);
+		
+						objectToControl.transform.Translate (movementVector * speed * Time.deltaTime * Time.timeScale);
+				}
+		}
+}
