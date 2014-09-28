3e7eb026f3e9d7de67d7c0ddc1bbfe82f51338b3
41407 <jimi.welling@cs.helsinki.fi>
2014-09-28 12:43:15 +0300

    Fix LookAtObject

--- flaming-flying-machine/Assets/Scripts/LookAtObject.cs
+++ flaming-flying-machine/Assets/Scripts/LookAtObject.cs
@@ -4,21 +4,11 @@ using System.Collections;
 public class LookAtObject : MonoBehaviour
 {
 		public GameObject objectToLookAt;
-
-		// Use this for initialization
-		void Start ()
-		{
 	
-		}
-	
-		// Update is called once per frame
 		void Update ()
 		{
-				Vector3 moveDirection = objectToLookAt.transform.position;
-				if (moveDirection != Vector3.zero) {
-						float angle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg - 90;
-						transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
-				}
-	
+				Vector3 relativeLookDirection = gameObject.transform.position - objectToLookAt.transform.position;
+				float angle = Mathf.Atan2 (relativeLookDirection.y, relativeLookDirection.x) * Mathf.Rad2Deg + 90;
+				transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
 		}
 }
