8d53874054d0d1961d502e4636033d543299b5eb
41407 <jimi.welling@cs.helsinki.fi>
2014-09-28 14:43:33 +0300

    Updated player prefab

---
+++ flaming-flying-machine/Assets/PlayerFiring.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFiring : MonoBehaviour
+{
+
+		public GameObject bullet;
+		public int rateOfFire;
+		private bool firing = false;
+		private int firingCooldown;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Input.GetMouseButtonDown (0)) {
+						print ("FIREIN'G");
+						firing = true;
+				} 
+				if (Input.GetMouseButtonUp (0)) {
+						firing = false;
+				}
+				if (firing && firingCooldown >= rateOfFire) {
+						firingCooldown = 0;
+						GameObject newBullet = (GameObject)Instantiate (bullet, this.gameObject.transform.position, new Quaternion ());
+						Physics2D.IgnoreCollision (newBullet.collider2D, gameObject.collider2D);
+				}
+		}
+}
