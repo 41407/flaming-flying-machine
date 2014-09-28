c5e842454b122f97fcd8cabc238a09faeb987236
Joni Kopra <joni.kopra@gmail.com>
2014-09-28 21:53:42 +0300

    Add rotation scripts

---
+++ flaming-flying-machine/Assets/CreateRotatorChilds.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateRotatorChilds : MonoBehaviour {
+
+	public GameObject rotatorChild;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 1; i < 4; i++) {
+			GameObject child = (GameObject) Instantiate(rotatorChild, gameObject.transform.position, Quaternion.identity);
+			child.GetComponent<OrbitAroundParent>().index = i;
+			child.GetComponent<OrbitAroundParent>().speed = 0.05f;
+			child.GetComponent<OrbitAroundParent>().radius = 0.5f;
+			child.GetComponent<OrbitAroundParent>().parent = gameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ flaming-flying-machine/Assets/OrbitAroundParent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitAroundParent : MonoBehaviour {
+
+	public GameObject parent;
+	public float speed;
+	public float radius;
+	public int index;
+
+	private float position;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float xPos = parent.transform.position.x + (Mathf.Sin (position + (2*(float)index/3.0f)*Mathf.PI) * radius);
+		float yPos = parent.transform.position.y + (Mathf.Cos (position + (2*(float)index/3.0f)*Mathf.PI) * radius);
+
+		gameObject.transform.position = new Vector3(xPos,yPos,0);
+
+		position += speed;
+	}
+}
