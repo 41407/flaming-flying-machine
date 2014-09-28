0a35670ac77be885a45127876159f6439b3ea457
41407 <jimi.welling@cs.helsinki.fi>
2014-09-28 12:07:39 +0300

    Add 2d look at script

---
+++ flaming-flying-machine/Assets/LookAtTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtTest : MonoBehaviour
+{
+		public GameObject objectToLookAt;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				Vector3 moveDirection = objectToLookAt.transform.position;
+				if (moveDirection != Vector3.zero) {
+						float angle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg - 90;
+						transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+				}
+	
+		}
+}
