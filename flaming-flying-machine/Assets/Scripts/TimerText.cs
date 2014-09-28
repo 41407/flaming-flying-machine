db07424f151444349f99a36c90bb273772fddf01
41407 <jimi.welling@cs.helsinki.fi>
2014-09-28 19:10:56 +0300

    Music testing

--- flaming-flying-machine/Assets/Scripts/TimerText.cs
+++ flaming-flying-machine/Assets/Scripts/TimerText.cs
@@ -3,16 +3,27 @@ using System.Collections;
 
 public class TimerText : MonoBehaviour {
 
-
+	private const float tick = 117.1875f;
+	private float timeBetweenTicks;
 	private float timer;
+	private float beatTimer;
+
 	// Use this for initialization
 	void Start () {
 		timer = 0.0f;
+		beatTimer = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime;
-		guiText.text = timer.ToString();
+		timeBetweenTicks += Time.deltaTime*1000;
+		if (timeBetweenTicks >= tick) {
+			beatTimer += 0.0625f;		
+			timeBetweenTicks -= tick;
+			audio.Play();
+		}
+
+		guiText.text = ("Sec: " + timer.ToString() + "\nBeat: " + beatTimer.ToString());
 	}
 }
