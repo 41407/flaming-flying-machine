b441c97a9f1ad9d70cccf40ca7b86dbd1c6fd653
41407 <jimi.welling@cs.helsinki.fi>
2014-10-04 20:17:14 +0300

    Glowy effects

---
+++ flaming-flying-machine/Assets/Scripts/AudioPitchRandomization.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPitchRandomization : MonoBehaviour
+{
+
+		public float amount = 0;
+
+		// Use this for initialization
+		void Start ()
+		{
+				if (gameObject.audio) {
+						gameObject.audio.pitch = Mathf.Min (1 + Random.Range (-amount, amount));
+				}
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
