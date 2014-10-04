34f3f498eefe0cef1c6fed15c871900c4cc10023
41407 <jimi.welling@cs.helsinki.fi>
2014-10-04 16:04:29 +0300

    Start boss script

---
+++ flaming-flying-machine/Assets/Boss.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : MonoBehaviour {
+
+	public Component[] stages;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.frameCount % 120 == 0) {
+			MonoBehaviour curr = (MonoBehaviour)stages[0];
+			curr.enabled = true;
+		}
+		if(Time.frameCount % 240 == 0) {
+			MonoBehaviour curr = (MonoBehaviour)stages[0];
+			curr.enabled = false;
+			curr = (MonoBehaviour)stages[1];
+			curr.enabled = true;
+		}
+	}
+}
---
+++ flaming-flying-machine/Assets/CavemanTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CavemanTest : MonoBehaviour
+{
+
+		public string testField;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				print (testField);
+		}
+}
