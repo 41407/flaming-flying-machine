d8e032f9130b149753552c86911de98ebe7faf74
41407 <jimi.welling@cs.helsinki.fi>
2014-10-04 18:30:51 +0300

    Crappy boss movement

--- flaming-flying-machine/Assets/Level1BossMovement.cs
+++ flaming-flying-machine/Assets/Level1BossMovement.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Level1BossMovement : MonoBehaviour {
+public class Level1BossMovement : MonoBehaviour
+{
 
-	public Vector2 goToPosition;
+		public Vector2[] positions;
+		private int currentPositionIndex = 0;
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start ()
+		{
 	
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
-		gameObject.transform.position = Vector2.Lerp(gameObject.transform.position, goToPosition, 0.2f * Time.deltaTime);
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+				gameObject.transform.position = Vector2.Lerp (gameObject.transform.position, positions [currentPositionIndex], 0.5f * Time.deltaTime);
+				if (Vector2.Distance (gameObject.transform.position, positions [currentPositionIndex]) < 1.0f) {
+						currentPositionIndex++;
+				}
+				if (currentPositionIndex >= positions.Length) {
+						currentPositionIndex = 0;
+				}
+		}
 }
