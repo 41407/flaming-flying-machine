b021562427d51c1b1953a7330707d3224584a1bb
41407 <jimi.welling@cs.helsinki.fi>
2014-10-04 19:34:25 +0300

    Bullet glow

---
+++ flaming-flying-machine/Assets/ScaleFlicker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleFlicker : MonoBehaviour
+{
+
+		public float minimum;
+		public float variance;
+	
+		// Use this for initialization
+		void Start ()
+		{
+		
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				float randomRange = Random.value * (variance) + minimum;
+				this.transform.localScale = new Vector2(randomRange, randomRange);
+		}
+}
