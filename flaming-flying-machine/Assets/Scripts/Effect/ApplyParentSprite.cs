d82f59806e11a26daa4beeb965ec7ddc6f4f4554
41407 <jimi.welling@cs.helsinki.fi>
2014-10-11 17:39:18 +0300

    Trails to use particle emitters instead of constantly instantiating new objects

---
+++ flaming-flying-machine/Assets/Scripts/Effect/ApplyParentSprite.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyParentSprite : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject parent = transform.parent.gameObject;
+		renderer.material.mainTexture = parent.GetComponent<SpriteRenderer> ().sprite.texture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
