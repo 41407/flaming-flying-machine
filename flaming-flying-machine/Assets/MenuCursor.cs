79eebad4622bd14d7a3b6d0d43b4b8fe9f4acb83
41407 <jimi.welling@cs.helsinki.fi>
2014-10-12 18:52:12 +0300

    Working level selector

--- flaming-flying-machine/Assets/MenuCursor.cs
+++ flaming-flying-machine/Assets/MenuCursor.cs
@@ -19,6 +19,9 @@ public class MenuCursor : MonoBehaviour
 				if (Input.GetMouseButtonDown (0) && currentButton.Equals ("Button.Start")) {
 						cam.GetComponent<MenuCamera> ().state = 1;		
 				}
+				if (Input.GetMouseButtonDown (0) && currentButton.Equals ("Button.Level1")) {
+						Application.LoadLevel (1);	
+				}
 		}
 	
 		void OnTriggerEnter2D (Collider2D col)
