dce2090e8d961401ac009a10b842b4d934d30b37
41407 <jimi.welling@cs.helsinki.fi>
2014-10-12 17:08:45 +0300

    Bullet sparks

---
+++ flaming-flying-machine/Assets/PlayerBulletDestruction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBulletDestruction : MonoBehaviour
+{
+
+		public GameObject explosion;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnDestroy ()
+		{
+				Destroy (Instantiate (explosion, transform.position, Quaternion.identity), 3.0f);
+		}
+}
--- flaming-flying-machine/Assets/Scripts/EnemyScripts/EnemyDestruction.cs
+++ flaming-flying-machine/Assets/Scripts/EnemyScripts/EnemyDestruction.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class EnemyDestruction : MonoBehaviour
 {
+		public GameObject hitEffect;
 		public GameObject explosion;
 		public int health;
 		public bool destroysBulletsOnImpact;
@@ -19,7 +20,9 @@ public class EnemyDestruction : MonoBehaviour
 		{
 				if (coll.gameObject.tag == "PlayerBullet") {
 						health -= coll.gameObject.GetComponent<BulletProperties> ().damage;
-						if (destroysBulletsOnImpact) { 
+						if (destroysBulletsOnImpact) {
+								Destroy (Instantiate (hitEffect, coll.gameObject.transform.position, Quaternion.identity), 3.0f);
+
 								Destroy (coll.gameObject);
 						}
 				}
