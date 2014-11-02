252c3e9c9f1d7a4b045464b6a69598e9b10058db
41407 <jimi.welling@cs.helsinki.fi>
2014-11-02 18:55:48 +0200

    Parse time into minutes, seconds and hundredths

--- flaming-flying-machine/Assets/TimeDisplay.cs
+++ flaming-flying-machine/Assets/TimeDisplay.cs
@@ -5,6 +5,20 @@ public class TimeDisplay : MonoBehaviour
 {
 		void Update ()
 		{
-				GetComponent<TextMesh> ().text = "" + GameStats.getTime ();
+				float decimalTime = GameStats.getTime ();
+				int seconds = (int)decimalTime;
+				int hundredths = (int)(decimalTime * 100) - seconds * 100;
+				int minutes = seconds / 60;
+				seconds = seconds - 60 * minutes;
+				string secondsLeadingZero = "";
+				if (seconds < 10) {
+						secondsLeadingZero += "0";
+				}
+				string hundredthsLeadingZero = "";
+				if (hundredths < 10) {
+						hundredthsLeadingZero += "0";
+				}
+				string parsedTime = minutes + ":" + secondsLeadingZero + seconds + "." + hundredthsLeadingZero + hundredths; 
+				GetComponent<TextMesh> ().text = parsedTime;
 		}
 }
